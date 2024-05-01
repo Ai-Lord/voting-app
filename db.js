c5ff05271120af0c4bf61b31760da6c979f601c6
Aashish Dewangan <aashishdew27@gmail.com>
2024-05-01 23:08:49 +0530

    url change

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 // define the mongodb connection url
-const mongoUrl = process.env.DB_URL_LOCAL;
-//const mongoUrl = process.env.DB_URL;
+//const mongoUrl = process.env.DB_URL_LOCAL;
+const mongoUrl = process.env.DB_URL;
 // replace 'mydatabase' with your databas e name
 
 // set up mongodb connection
